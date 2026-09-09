c16d362c23f0937d2b6c442d0765973e70da63e5
Liv <livvya@rape.ink>
2026-09-09 14:53:49 +0200

    routes..

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,16 @@
-<script>
-import Home from "./views/Home.svelte"
+<script lang="ts">
+    import { createRouter, View } from "@shaun/svelte-router";
+    import Home from "./views/Home.svelte";
+    import Panel from "./views/Panel.svelte";
+
+    createRouter({
+        mode: "web",
+        routes: [
+            { path: "/", component: Home },
+            { path: "/panel", component: Panel },
+            { path: "*", component: Home }
+        ]
+    });
 </script>
-<Home/>
+
+<View />
---
+++ src/views/Panel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<div class="panel_main">
+    <h1>Panel</h1>
+</div>
+
+<style>
+    .panel_main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        color: #ffffff;
+        background: #111111;
+    }
+</style>
